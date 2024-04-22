c3497bd43529d4f9d926bc85f5034b7c2ccc31a4
DemoAccount002 <167759565+DemoAccount002@users.noreply.github.com>
2024-04-22 11:39:36 +0100

    Update movement.cs
    added a commit

--- test project/Assets/scripts/movement.cs
+++ test project/Assets/scripts/movement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//i have just did a small edit on github to show how to pull chenges made on a different computer
 public class movement : MonoBehaviour
 {
     // Start is called before the first frame update
